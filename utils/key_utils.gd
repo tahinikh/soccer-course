ed2e73cec4b19026d3c73f6a70e09977f20d0dcb
tahinikh <fatemehazam460@gmail.com>
2025-08-07 13:57:27 -0500

    Update key_utils.gd

--- utils/key_utils.gd
+++ utils/key_utils.gd
@@ -1,1 +1,29 @@
 extends Node
+
+enum Action {LEFT, RIGHT, UP, DOWN, SHOOT, PASS}
+
+const ACTIONS_MAP : Dictionary = {
+	Player.ControlScheme.P1: {
+		Action.LEFT: "p1_left",
+		Action.RIGHT: "p1_right",
+		Action.UP: "p1_up",
+		Action.DOWN: "p1_down",
+		Action.SHOOT: "p1_shoot",
+		Action.PASS: "p1_pass",
+	},
+	Player.ControlScheme.P2: {
+		Action.LEFT: "p2_left",
+		Action.RIGHT: "p2_right",
+		Action.UP: "p2_up",
+		Action.DOWN: "p2_down",
+		Action.SHOOT: "p2_shoot",
+		Action.PASS: "p2_pass",
+	},
+}
+
+func get_input_vector(scheme: Player.ControlScheme) -> Vector2
+	var map : Dictionary = ACTIONS_MAP[scheme]	
+	return Input.get_vector(map[Action.LEFT], map[Action.RIGHT], map[Action.UP], map[Action.DOWN])
+	
+	
+		
